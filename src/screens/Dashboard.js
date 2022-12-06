167d4a3cbe29a6de47faeaa84fb01e5a840b5705
Bahadır Aksakal <72493865+bahadraksakal@users.noreply.github.com>
2022-12-06 22:56:38 +0300

    AsyncStorage ile kullanıcı bilgileri alma eklendi

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Background from '../components/Background'
 import Header from '../components/Header'
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 
 export default function Dashboard({ navigation }) {
+  const [userName,setUserName]=useState("");
+  const [userMail,setUserMail]=useState("");
+  AsyncStorage.getItem('userInfo').then(value=>{
+    var data=JSON.parse(value);
+    setUserMail(data.user.userName);
+    setUserName(data.user.userMail);
+    //console.log("userInfo:"+value);    
+  });     
+  
   return (
+    
     <Background>
+      <Paragraph>Ad:{userName}</Paragraph>
+      <Paragraph>Mail: {userMail}</Paragraph>
       <Header> Giriş yapıldı</Header>
       <Paragraph>
         çıkış yapmak için butona tıklayınız.
